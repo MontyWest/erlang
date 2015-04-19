5180858cd265c03bb39d30b12f188b43a3975f53
MontyWest <monty.p.west@gmail.com>
2015-04-19 17:10:02 +0100

    display temp actor start

---
+++ displayTemperature.erl
@@ -0,0 +1,13 @@
+-module(displayTemperature).
+-export([loop/0]).
+
+loop() ->
+	receive
+		{temperature,{C, F}} ->
+			io.fwrite("Celcius: ~g, Fahrenheit: ~g ~n"),
+			loop();
+		_ ->
+			io:fwrite("Can't display~n"),
+			loop()
+	end.
+
